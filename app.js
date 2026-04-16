3bddaf6e1e107d8628390e47521fc52aee8413d7
Alsakkaf.dev <mohammed.alsakkaaf@gmail.com>
2026-04-16 13:14:40 +0800

    Exercise 1: Create and verify custom personal JSON object

--- app.js
+++ app.js
@@ -30,3 +30,23 @@ console.log("The stringified version:", resultString);
 
 // Pretty-print version (makes it easier to read in the console)
 console.log(JSON.stringify(myData, null, 2));
+
+// --- Exercise 1 ---
+
+// 1. Creating the JavaScript Object (about myself)
+const aboutMe = {
+    "fullName": "Mohammed_Alsakkaf",
+    "age": 23,
+    "hobbies": ["Coding", "Gaming", "Reading"],
+    "address": {
+        "city": "Johor Bahro",
+        "country": "Malaysia"
+    }
+};
+
+// 2. Verifying by converting it to string
+const myJson = JSON.stringify(aboutMe);
+console.log("Exercise 1 JSON:", myJson);
+
+// 3. Verifying by parsing it back
+console.log("Verified Object:", JSON.parse(myJson));
