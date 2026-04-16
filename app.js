3aac4f82ab6d3f1812b4c2a16638522c98e5aac9
Alsakkaf.dev <mohammed.alsakkaaf@gmail.com>
2026-04-16 13:11:21 +0800

    Part 1.5: Use JSON.parse() to read the student data

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// Convert the string into a JavaScript Object
+const studentObj = JSON.parse(jsonString);
+
+// Print specific properties to the console
+console.log("Name from JSON:", studentObj.name);
+console.log("First Subject:", studentObj.subjects[0]);
+
 // A JSON string representing a student
 const jsonString = `{
     "name": "Ahmad Hafiz",
